f7064d218854688ea93d3ab4ae9bfbd63f0a628a
Jonathan Rajavuori <jrajav@gmail.com>
2020-02-23 15:54:56 -0600

    Add blank rustfmt.toml (to signal that auto-formatting is in fact used), run cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -9,30 +9,31 @@ pub mod mapd;
 pub mod serialized_result_set;
 
 pub mod client {
-  use crate::mapd::MapDSyncClient;
+    use crate::mapd::MapDSyncClient;
 
-  use thrift::protocol::{TBinaryInputProtocol, TBinaryOutputProtocol};
-  use thrift::transport::{
-    ReadHalf, TBufferedReadTransport, TBufferedWriteTransport, TIoChannel, TTcpChannel, WriteHalf,
-  };
+    use thrift::protocol::{TBinaryInputProtocol, TBinaryOutputProtocol};
+    use thrift::transport::{
+        ReadHalf, TBufferedReadTransport, TBufferedWriteTransport, TIoChannel, TTcpChannel,
+        WriteHalf,
+    };
 
-  pub fn create(
-    remote_address: &str,
-  ) -> Result<
-    MapDSyncClient<
-      TBinaryInputProtocol<TBufferedReadTransport<ReadHalf<TTcpChannel>>>,
-      TBinaryOutputProtocol<TBufferedWriteTransport<WriteHalf<TTcpChannel>>>,
-    >,
-    thrift::Error,
-  > {
-    let mut c = TTcpChannel::new();
-    c.open(remote_address)?;
+    pub fn create(
+        remote_address: &str,
+    ) -> Result<
+        MapDSyncClient<
+            TBinaryInputProtocol<TBufferedReadTransport<ReadHalf<TTcpChannel>>>,
+            TBinaryOutputProtocol<TBufferedWriteTransport<WriteHalf<TTcpChannel>>>,
+        >,
+        thrift::Error,
+    > {
+        let mut c = TTcpChannel::new();
+        c.open(remote_address)?;
 
-    let (i_chan, o_chan) = c.split()?;
+        let (i_chan, o_chan) = c.split()?;
 
-    let i_prot = TBinaryInputProtocol::new(TBufferedReadTransport::new(i_chan), true);
-    let o_prot = TBinaryOutputProtocol::new(TBufferedWriteTransport::new(o_chan), true);
+        let i_prot = TBinaryInputProtocol::new(TBufferedReadTransport::new(i_chan), true);
+        let o_prot = TBinaryOutputProtocol::new(TBufferedWriteTransport::new(o_chan), true);
 
-    Ok(MapDSyncClient::new(i_prot, o_prot))
-  }
+        Ok(MapDSyncClient::new(i_prot, o_prot))
+    }
 }
